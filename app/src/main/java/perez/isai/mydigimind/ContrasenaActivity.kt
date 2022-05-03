86d543eb5f8323bb9bfe50fbe5d1ddc200e31f5c
Isai Perez <72577853+alfredo2201@users.noreply.github.com>
2022-05-03 15:15:54 -0700

    actualizacion

---
+++ app/src/main/java/perez/isai/mydigimind/ContrasenaActivity.kt
@@ -0,0 +1,11 @@
+package perez.isai.mydigimind
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContrasenaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contrasena)
+    }
+}
---
+++ app/src/main/java/perez/isai/mydigimind/RegistroActivity.kt
@@ -0,0 +1,11 @@
+package perez.isai.mydigimind
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+}
